c18e9b896ab058cb938aafa5ca265f1548ec75c8
richardsforu <praveen.somireddy@gmail.com>
2018-12-04 15:05:43 +0530

    JS Files Added

---
+++ Java Script/1-datatypes.js
@@ -0,0 +1,60 @@
+console.log('-- data types');
+
+/*
+
+---------------------
+Data types
+---------------------
+
+1. simple/primitive  ==> values
+    a. undefined
+    b. string
+    c. number
+    d. boolean
+
+*/
+
+// 1. Simple Types
+// a. undefined
+
+var num;
+//------------------------
+
+// b. string
+
+var name='Praveen';
+var s1="James";
+
+var dynamicString='The Trainer is '+name;
+
+var dynamicString2=`the trainer ${name}`;
+
+var result=`the sum of 1 and 2 is ${1+2}`;
+
+var multiLineString=`
+
+line -1
+line -2
+line -3
+
+`;
+
+var htmltemplate=`
+
+<div>
+    <h1>Name is ${name} <h1>
+</div>
+`;
+
+//-------------------------------
+// c. number
+//--------------------------------
+
+var count=12;
+var cost=34.87;
+
+// ------------------------------
+// d. boolean 
+//-------------------------------
+
+
